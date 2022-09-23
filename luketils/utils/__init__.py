0cf250b5d12ad519a1dbb99a52970ee17c983e58
Luke Wood <lukewoodcs@gmail.com>
2022-09-23 15:28:12 -0700

    Add experiment_name(base) function

--- luketils/utils/__init__.py
+++ luketils/utils/__init__.py
@@ -1,6 +1,10 @@
 import os
-
+from datetime import datetime
 
 def ensure_exists(path):
     """ensure a nested directory exists."""
      os.makedirs(path, exist_ok=True)
+
+def experiment_name(base):
+    now = datetime.now()
+    return f"{base}-{now.strftime('%m/%d/%y-%H:%M%S')}"
