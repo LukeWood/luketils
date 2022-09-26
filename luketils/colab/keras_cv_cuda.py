3254993cd0d82b99b4d8c99f1c4c0af6c8b273ef
Luke Wood <lukewoodcs@gmail.com>
2022-09-26 11:59:04 -0700

    Rename function

--- luketils/colab/keras_cv_cuda.py
+++ luketils/colab/keras_cv_cuda.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def keras_cv_cuda():
+def install_keras_cv_cuda():
     """Update the CUDA kernel in your Colab environment to support running KerasCV."""
     subprocess.run(
         [
